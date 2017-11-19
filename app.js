9476e067b1268e7f44df6791739f646e29a1849e
weswhitney <wesleywhitney@gmail.com>
2017-11-18 21:28:11 -0700

    added fall through route and server listen log

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ app.get('/', function (req, res) {
     res.send('hello world');
 });
 
+app.use(function (req, res) {
+    res.sendStatus(404);
+});
+
 var port;
 port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port, function () {
+    console.log('Express server listening on ', port);
+});
 
