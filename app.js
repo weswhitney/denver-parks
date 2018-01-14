c32726b19562dbe663093f8f61865936a677d5e1
weswhitney <wesleywhitney@gmail.com>
2018-01-14 11:05:22 -0700

    es 2015 declarations

--- app.js
+++ app.js
@@ -9,12 +9,11 @@ app.engine('html', engines.nunjucks);// register the nunjucks template, associat
 app.set('view engine', 'html');// set the view engine app setting to html meaning were going to use the nunjucks engine to render html
 app.set('views', __dirname + '/views'); // specify where our templates are located
 
-var herokuUri;
-var localUri;
-localUri = 'mongodb://localhost:27017/parks';
-herokuUri = 'mongodb://heroku_vxdmqrm5:h2dh7j5i28ljk7fq86nubhpk1v@ds113566.mlab.com:13566/heroku_vxdmqrm5';
 
-MongoClient.connect(localUri, function(err, db) {
+const localUri = 'mongodb://localhost:27017/parks';
+const herokuUri = 'mongodb://heroku_vxdmqrm5:h2dh7j5i28ljk7fq86nubhpk1v@ds113566.mlab.com:13566/heroku_vxdmqrm5';
+
+MongoClient.connect(herokuUri, function(err, db) {
     app.get('/',function (req, res) {
         res.render('hello', {name: 'templates'});
     });
@@ -33,7 +32,7 @@ MongoClient.connect(localUri, function(err, db) {
         res.sendStatus(404);
     });
 
-    var port;
+    let port;
     port = process.env.PORT || 3000;
     app.listen(port, function () {
     console.log('Express server listening on ', port);
