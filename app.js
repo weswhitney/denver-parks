86e031209b9decda0673f779a5e1dc9d26b7d410
weswhitney <wesleywhitney@gmail.com>
2018-01-15 22:10:48 -0700

    commented out local uri for mongodb

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
-const engines = require('consolidate'); // consolidate is a wrapper for template interfaces
+// consolidate is a wrapper for template interfaces
+const engines = require('consolidate');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+/*
+ TODO see if there is a better way to associate the pug template.
+ register the pugs template, associating it with the pug extensions
+ */
+app.engine('pug', require('pug').__express);
+// set the view engine app setting to pug meaning were going to use the pug engine to render html
+app.set('view engine', 'pug');
+// specify where our templates are located
+app.set('views', __dirname + '/views');
 
-app.engine('pug', require('pug').__express);// register the nunjucks template, associating it with the html extensions
-app.set('view engine', 'pug');// set the view engine app setting to html meaning were going to use the nunjucks engine to render html
-app.set('views', __dirname + '/views'); // specify where our templates are located
-
-
-const localUri = 'mongodb://localhost:27017/parks'; // don't need this anymore because it hits the prod heroku db.
+/*
+ don't need this anymore because it hits the prod heroku db.
+ const localUri = 'mongodb://localhost:27017/parks';
+ */
 const herokuUri = 'mongodb://heroku_vxdmqrm5:h2dh7j5i28ljk7fq86nubhpk1v@ds113566.mlab.com:13566/heroku_vxdmqrm5';
 
 MongoClient.connect(herokuUri, (err, db) => {
