ff56a3b16fc81e610519e08dbf4fe8dc4bd6ec39
weswhitney <wesleywhitney@gmail.com>
2018-02-01 22:30:42 -0700

    webpack with react working but with many errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 const config = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         filename: 'bundle.js',
         path: __dirname + '/dist'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/',
+                query: {
+                    presets: ['env', 'react']
+                }
+            }
+        ]
     }
 };
 
