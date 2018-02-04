020fd677778257480052197ad32a0b0a04fc5609
weswhitney <wesleywhitney@gmail.com>
2018-02-03 23:11:22 -0700

    first component working

---
+++ client/src/Playgrounds.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Playgrounds extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Firsties</h1>
+            </div>
+        );
+    }
+}
+
+export default Playgrounds;
